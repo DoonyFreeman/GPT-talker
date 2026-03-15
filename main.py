ed0d3fc7740be9a04b18979fee329de10e828634
DoonyFreeman <rebrikov2006@gmail.com>
2026-03-15 17:36:07 +0300

    added Body for JSON

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
+from gemini_client import get_answer_from_gemini
 
 app = FastAPI()
 
 @app.get("/requests")
 def get_my_requests():
     return "Here are your requests!"
+
+@app.post("/requests")
+def send_prompt(
+    prompt: str = Body(embed=True,)
+):
+    answer = get_answer_from_gemini(prompt)
+    return {"answer": answer}
